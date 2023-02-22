e892515fb1511e59044ab5b696ff06daf5c0644c
sorakanda <115918500+sorakanda@users.noreply.github.com>
2023-02-22 13:09:37 +0900

    commit6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   
+=======
+  devise_for :users
+  root "items#index"
+  resources :items
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
