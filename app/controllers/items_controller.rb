19757557ab8f9940897044919021d9e72c9a3888
sorakanda <115918500+sorakanda@users.noreply.github.com>
2023-02-26 17:44:04 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
     
   end
 
   def new
-    if user_signed_in?
       @item = Item.new
-    else
-      render 'devise/sessions/new'
-    end
   end
 
   def create
@@ -16,7 +13,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render new_item_path
+      render :new
     end
   end
 
