c13839a1c51277aa020383d8e0352fee7d0f7ffd
sorakanda <115918500+sorakanda@users.noreply.github.com>
2023-03-18 16:02:17 +0900

    b

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
+    
     if @order_address.valid?
       pay_item
       @order_address.save
